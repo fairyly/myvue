7c3f213a221c47ca7b31e5b35a667e6684163268
fairy <fairyilys@gmail.com>
2018-05-05 17:48:02 +0800

    Create NUXT.js

---
+++ NUXT.js
@@ -0,0 +1,15 @@
+# nuxt
+
+* doc: https://zh.nuxtjs.org/guide/configuration
+* github: https://github.com/nuxt/nuxt.js
+
+### install
+```
+  npm install -g vue-cli
+  vue init nuxt-community/starter-template <project-name>
+  cd <project-name>
+  npm install
+  启动项目：
+  npm run dev
+
+```
