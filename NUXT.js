0f46448838962cf2c9612467a90730bf65ea6dfd
fairy <fairyilys@gmail.com>
2018-05-05 17:49:16 +0800

    Update NUXT.js

--- NUXT.js
+++ NUXT.js
@@ -6,10 +6,16 @@
 ### install
 ```
   npm install -g vue-cli
+
   vue init nuxt-community/starter-template <project-name>
+
   cd <project-name>
+
+  安装依赖：
   npm install
+
   启动项目：
   npm run dev
 
+  应用现在运行在 http://localhost:3000
 ```
