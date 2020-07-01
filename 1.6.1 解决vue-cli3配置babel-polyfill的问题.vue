47766be0b272628282068e32523ee7e99d7aa689
fairy <fairyilys@gmail.com>
2020-07-01 16:21:12 +0800

    Create 1.6.1 解决vue-cli3配置babel-polyfill的问题.vue

---
+++ 1.6.1 解决vue-cli3配置babel-polyfill的问题.vue
@@ -0,0 +1,33 @@
+# 1.6.1 解决vue-cli3配置babel-polyfill的问题
+
+
+## install
+
+```
+  npm i babel-polyfill --save-dev
+```
+
+## 引入
+
+```
+有的说在main.js中，import 'babel-polyfill' 引入,测试后无效。
+
+注意一下 node_module中 polyfill 的文件路径，有的可能是 import '@babel/polyfill' 这种情况。
+
+在vue.config.js 中 configureWebpack 字段中，具体：
+
+ chainWebpack: config => {
+    config.entry.app = ["babel-polyfill", "./src/main.js"];
+ }
+ 
+ 链式webpack配置函数
+
+chainWebpack (config) {
+  config.entry('main').add('babel-polyfill')
+}
+
+```
+
+
+## 参考
+- https://cli.vuejs.org/zh/guide/webpack.html
