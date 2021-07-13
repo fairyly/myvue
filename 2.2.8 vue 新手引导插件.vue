8f691e2af7bfe4758956138736b080315f03a0d3
fairy <fairyilys@gmail.com>
2021-07-13 15:05:23 +0800

    Create 2.2.8 vue 新手引导插件.vue

---
+++ 2.2.8 vue 新手引导插件.vue
@@ -0,0 +1,70 @@
+# 2.2.8 vue 新手引导插件
+
+## install
+
+```
+npm install vue-tour
+
+
+import Vue from 'vue'
+import App from './App.vue'
+import VueTour from 'vue-tour'
+
+require('vue-tour/dist/vue-tour.css')
+
+Vue.use(VueTour)
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
+```
+- demo
+
+```
+<template>
+  <div>
+    <div id="v-step-0">A DOM element on your page. The first step will pop on this element because its ID is 'v-step-0'.</div>
+    <div class="v-step-1">A DOM element on your page. The second step will pop on this element because its ID is 'v-step-1'.</div>
+    <div data-v-step="2">A DOM element on your page. The third and final step will pop on this element because its ID is 'v-step-2'.</div>
+
+    <v-tour name="myTour" :steps="steps"></v-tour>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-tour',
+    data () {
+      return {
+        steps: [
+          {
+            target: '#v-step-0',  // We're using document.querySelector() under the hood
+            header: {
+              title: 'Get Started',
+            },
+            content: `Discover <strong>Vue Tour</strong>!`
+          },
+          {
+            target: '.v-step-1',
+            content: 'An awesome plugin made with Vue.js!'
+          },
+          {
+            target: '[data-v-step="2"]',
+            content: 'Try it, you\'ll love it!<br>You can put HTML in the steps and completely customize the DOM to suit your needs.',
+            params: {
+              placement: 'top' // Any valid Popper.js placement. See https://popper.js.org/popper-documentation.html#Popper.placements
+            }
+          }
+        ]
+      }
+    },
+    mounted: function () {
+      this.$tours['myTour'].start()
+    }
+  }
+</script>
+```
+
+
+## 参考
+- [pulsardev/vue-tour](https://github.com/pulsardev/vue-tour)
